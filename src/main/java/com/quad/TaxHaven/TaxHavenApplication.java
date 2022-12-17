7b20e1cfd1f04b2f96c3f8fad2b6289a47a17694
rawal_kumar <rawal_kumar@epam.com>
2022-12-18 00:08:13 +0530

    application created

---
+++ src/main/java/com/quad/TaxHaven/TaxHavenApplication.java
@@ -0,0 +1,14 @@
+package com.quad.TaxHaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxHavenApplication.class, args);
+		System.out.println("Sri Ganesh");
+	}
+
+}
---
+++ src/test/java/com/quad/TaxHaven/TaxHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quad.TaxHaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
